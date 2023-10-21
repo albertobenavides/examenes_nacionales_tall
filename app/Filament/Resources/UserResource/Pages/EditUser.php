2ca357c8c8a9e5b45b54d07e3960af6e2b0e79d1
Alberto Benavides <bena.87@hotmail.com>
2023-10-21 12:36:44 -0600

    Sin vista de alumno

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -14,7 +14,7 @@ class EditUser extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\ViewAction::make(),
+            // Actions\ViewAction::make(),
             Actions\DeleteAction::make(),
         ];
     }
