9ccbf0f955afd5a51ef675c00358d971ecf61f20
Alberto Benavides <bena.87@hotmail.com>
2024-01-08 12:45:35 -0600

    Actualizado tipo de dato

--- database/migrations/2024_01_08_120240_add_notes_users_migration.php
+++ database/migrations/2024_01_08_120240_add_notes_users_migration.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('notes')->nullable();
+            $table->text('notes')->nullable();
         });
     }
 
