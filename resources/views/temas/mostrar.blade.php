584f06b3044b60f454a45fa78fccb9d988e06cef
Alberto Benavides <bena.87@hotmail.com>
2024-03-05 15:29:50 -0600

    Nombre corregido

--- resources/views/temas/mostrar.blade.php
+++ resources/views/temas/mostrar.blade.php
@@ -120,7 +120,7 @@
                 <hr>
                 @if ($tema->contenido != null)
                     @for ($i = 0; $i < count($tema->contenido); $i++)
-                        <h2 class="lead mt-3" id='embebed-{{ $i }}'>{{ $tema->contenido[$i]['data']['titulo'] ?? '' }}</h2>
+                        <h2 class="lead mt-3" id='embebed-{{ $i }}'>{{ $tema->contenido[$i]['data']['titulo'] ?? 'Sección ' . $i + 1 }}</h2>
                         @if ($tema->contenido[$i]['type'] == 'texto')
                             {!! $tema->contenido[$i]['data']['texto'] !!}
                         @elseif ($tema->contenido[$i]['type'] == 'h5p')
