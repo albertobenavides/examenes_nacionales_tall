2f592b10374b4810040dca60b61c418b91448378
Alberto Benavides <bena.87@hotmail.com>
2024-03-08 08:24:13 -0600

    Sin búsqueda, ahora sí

--- resources/views/layouts/new_app.blade.php
+++ resources/views/layouts/new_app.blade.php
@@ -49,12 +49,12 @@
             </div>
         </div>
         <div class="navbar-center">
-            <label class="input input-bordered flex items-center">
+            {{-- <label class="input input-bordered flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                 </svg>
-                {{-- <input type="text" placeholder="Buscar... Próximamente" disabled /> --}}
-            </label>
+                <input type="text" placeholder="Buscar... Próximamente" disabled />
+            </label> --}}
             @auth
             <ul class="menu menu-horizontal px-1">
                 <li><a href="/cursos" class="btn btn-ghost text-xl hidden sm:flex">Mis cursos</a></li>
