850d6bcca72174ba803f6cf399be3f8c92c939c6
Alberto Benavides <bena.87@hotmail.com>
2024-03-14 18:15:33 -0600

    Sin disparar

--- resources/views/ejercicios/mostrar.blade.php
+++ resources/views/ejercicios/mostrar.blade.php
@@ -126,7 +126,6 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
             </svg>
         </button>
-        <button onclick="shoot()">disparar</button>
     </div>
 @endsection
 
