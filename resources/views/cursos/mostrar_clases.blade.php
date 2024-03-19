72fe1478ee09078e2e3d343fb1e4c6b5ffaf345a
Alberto Benavides <bena.87@hotmail.com>
2024-03-19 17:48:48 -0600

    Meetings agregados en cursos

--- resources/views/cursos/mostrar_clases.blade.php
+++ resources/views/cursos/mostrar_clases.blade.php
@@ -28,7 +28,7 @@
                     @endphp
                 @endforeach
                 {{-- [ ] Actualizar URLs --}}
-                <a href="/modulos/{{ $m->id }}" class="card lg:card-side bg-white shadow-xl my-3"> 
+                <a href="/modulos/{{ $m->id }}" class="card lg:card-side bg-white shadow-xl my-3">
                     <figure class="w-full lg:w-60">
                         @if ($m->imagen)
                             <img src="/storage/{{ $m->imagen }}">
@@ -56,6 +56,33 @@
         </div>
         <div class="lg:basis-1/4 ms-4">
             <h2 class="text-center"><b>Sesiones</b></h2>
+            @foreach ($curso->meetings as $meeting)
+                <div class="card shadow-lg my-3">
+                    <div class="card-title flex bg-primary text-white p-1 rounded-t-md">
+                        <div class="w-5">
+                            @if ($meeting->status == 'grabada')
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+                                </svg>
+                            @elseif($meeting->status == null)
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
+                                </svg>
+                            @else
+                            @endif
+                        </div>
+                        <div class="w-full">
+                            <h2 class="text-center">
+                                {{ $meeting->created_at }}
+                            </h2>
+                        </div>
+                    </div>
+                    <div class="card-body bg-white rounded-b-md">
+                        <p class="text-center">{{ $meeting->meetingName }}</p>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
